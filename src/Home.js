5da37e8b91ad4723cd79b4591bebc6fdc38ebca4
Saravanan S <ssslifeskills@gmail.com>
2022-07-20 16:52:42 +0530

    FS-2

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ export function Home() {
 
   return (
     <div className="home">
-      <h2>Welcome to Movie Reviewing WebApplication</h2>
+      <h2>Welcome to Movie Reviewing World</h2>
       <p>Here we help people to decide which Movies to watch by showing them the Ratings & Reviews, and Here's the interesting part , you can also Provide the Ratings & Reviews of the movies that you have watched. </p>
       <Button onClick={()=>{history.push("/movies")}} color="inherit">Click Here to Check Ratings and Reviews of Movies</Button>
       <Button onClick={()=>{history.push("/movie/add")}} color="inherit">Click Here to Rate and Review your Favorite Movie</Button>
